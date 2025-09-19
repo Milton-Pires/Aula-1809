d73e2351262d226c5a380da535be10c54f6f5e85
Milton-Pires <mpnn2207@gmail.com>
2025-09-18 22:15:57 -0300

    Atividade feita

--- app/src/main/java/br/com/etecia/myapp/CardActivity.java
+++ app/src/main/java/br/com/etecia/myapp/CardActivity.java
@@ -1,30 +1,24 @@
 package br.com.etecia.myapp;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-public class MenuPrincipalActivity extends AppCompatActivity {
-    MaterialCardView cdvPrimeiro = findViewById(R.id.cdvPrimeiro);
+
+public class CardActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.menu_principal_layout);
+        setContentView(R.layout.card_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
     }
-    cdvPrimeiro.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            startActivity(new Intent(getApplicationContext(), CardActivity.class ));
-        }
 }
